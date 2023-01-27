cd6009f793ae5aef666bedf789c84629b36fb80f
Vaishnav <patilvaishnav222@gmail.com>
2023-01-27 21:08:23 +0530

    SOP statement added

--- src/com/velocity/shopping/ProductList.java
+++ src/com/velocity/shopping/ProductList.java
@@ -3,8 +3,7 @@ package com.velocity.shopping;
 public class ProductList {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello");
 	}
 
 }
